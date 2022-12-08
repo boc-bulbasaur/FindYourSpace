ac757d6188f2f9cb55c667db95641529286e97b3
Keren Liu <karenliu816@gmail.com>
2022-12-08 18:27:24 -0500

    added reservation branch

---
+++ pages/reservation.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function NewReservation() {
+  return (
+    <>
+      <h1>CHECKOUT</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+
+}
