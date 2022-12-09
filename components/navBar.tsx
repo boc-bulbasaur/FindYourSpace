2932957acf9868ad13824312598435150a4f0cd7
Justo Miko Marquez <marquezmiko@gmail.com>
2022-12-08 21:11:52 -0500

    modified navbar

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -11,7 +11,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-// import AdbIcon from '@mui/icons-material/Adb';
 
 const pages = ['Reserve', 'Create', 'History'];
 const settings = ['Profile', 'Account', 'History', 'Login/Logout'];
@@ -39,7 +38,6 @@ function NavBar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
           <Typography
             variant="h6"
             noWrap
@@ -94,7 +92,6 @@ function NavBar() {
               ))}
             </Menu>
           </Box>
-          {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
           <Typography
             variant="h5"
             noWrap
