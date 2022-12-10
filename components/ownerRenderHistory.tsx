abecc45b18d563c626954f66dd5f7bd07ab1db3f
Eric <e.kalin12@gmail.com>
2022-12-10 01:32:05 -0500

    Added additional history columns and adjusted styling

--- components/ownerRenderHistory.tsx
+++ components/ownerRenderHistory.tsx
@@ -2,29 +2,33 @@ import * as React from 'react';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'id', headerName: 'Confirmation Code', width: 180 },
+  { field: 'fullName', headerName: 'Full Name', description: 'This column has a value getter and is not sortable.', width: 200 },
+  { field: 'startDateTime', type: 'dateTime', headerName: 'Start DateTime', width: 225 },
+  { field: 'endDateTime', type: 'dateTime', headerName: 'End DateTime', width: 225 },
   {
-    field: 'fullName',
-    headerName: 'Full Name',
-    description: 'This column has a value getter and is not sortable.',
-    width: 160,
+    field: 'duration',
+    headerName: 'Rental Duration',
+    width: 200,
+    valueGetter: (params) =>
+      // `${params.row.startDateTime || ''}`,
+      `${params.row.endDateTime.getHours() - params.row.startDateTime.getHours()} Hours`
   },
-  { field: 'duration', headerName: 'Rental Duration', width: 140, sortable: false},
-  { field: 'location', headerName: 'Location', width: 200, sortable: false},
-  { field: 'total', headerName: 'Total', width: 100 },
+  { field: 'location', headerName: 'Location', width: 300, sortable: false},
+  { field: 'total', headerName: 'Total', width: 128 },
   { field: 'block', headerName: 'Block User', width: 100, sortable: false, type: 'boolean' }
 
 ];
 
 const rows = [
-  { id: 1, fullName: 'Justo Marquez', duration: null, location: '1234 Example st', total: '$25.00', block: true },
-  { id: 2, fullName: 'Keren Liu', duration: null, location: '1234 Example st', total: '$25.00', block: null },
-  { id: 3, fullName: 'Eric Kalin', duration: null, location: '1234 Example st', total: '$25.00', block: null },
-  { id: 4, fullName: 'Keith Hall', duration: null, location: '1234 Example st', total: '$25.00', block: null },
-  { id: 5, fullName: 'Zefeng Shen', duration: null, location: '1234 Example st', total: '$25.00', block: null },
-  { id: 6, fullName: 'Miranda Zhou', duration: null, location: '1234 Example st', total: '$25.00', block: null },
-  { id: 7, fullName: 'Jewell Wilson', duration: null, location: '1234 Example st', total: '$25.00', block: null },
-  { id: 8, fullName: 'JT Liu', duration: null, location: '1234 Example st', total: '$25.00', block: null },
+  { id: 1, fullName: 'Justo Marquez', startDateTime: new Date('2022-04-20 8:00'), endDateTime: new Date('2022-04-20 12:00'), duration: null, location: '1234 Example st', total: '$32.00', block: true },
+  { id: 2, fullName: 'Keren Liu', startDateTime: new Date('2022-04-22 9:00'), endDateTime: new Date('2022-04-22 14:00'), duration: null, location: '1234 Example st', total: '$40.00', block: null },
+  { id: 3, fullName: 'Eric Kalin', startDateTime: new Date('2022-04-25 8:00'), endDateTime: new Date('2022-04-25 9:00'), duration: null, location: '1234 Example st', total: '$10.00', block: null },
+  { id: 4, fullName: 'Keith Hall', startDateTime: new Date('2022-04-26 12:00'), endDateTime: new Date('2022-04-26 13:00'), duration: null, location: '1234 Example st', total: '$10.00', block: null },
+  { id: 5, fullName: 'Zefeng Shen', startDateTime: new Date('2022-04-29 8:00'), endDateTime: new Date('2022-04-29 16:00'), duration: null, location: '1234 Example st', total: '$55.00', block: null },
+  { id: 6, fullName: 'Miranda Zhou', startDateTime: new Date('2022-04-23 11:00'), endDateTime: new Date('2022-04-23 17:00'), duration: null, location: '1234 Example st', total: '$45.00', block: null },
+  { id: 7, fullName: 'Jewell Wilson', startDateTime: new Date('2022-04-25 8:00'), endDateTime: new Date('2022-04-25 12:00'), duration: null, location: '1234 Example st', total: '$25.00', block: null },
+  { id: 8, fullName: 'JT Liu', startDateTime: new Date('2022-04-25 8:00'), endDateTime: new Date('2022-04-25 12:00'), duration: null, location: '1234 Example st', total: '$25.00', block: null },
 ];
 
 export default function O_RenderHistory() {
--- pages/ownerHistoryDash.tsx
+++ pages/ownerHistoryDash.tsx
@@ -1,9 +1,11 @@
+import NavBar from "../components/navBar"
 import O_RenderHistory from "../components/ownerRenderHistory"
 import DatePicker from "../components/ownerCalendarView"
 
 export default function OwnerHistory() {
   return (
     <>
+      <NavBar />
       <DatePicker />
       <O_RenderHistory />
     </>
