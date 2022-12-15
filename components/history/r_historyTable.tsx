0f3bfac9d312a11b50b478dc06f6fcd77d4dddbf
Justo Miko Marquez <marquezmiko@gmail.com>
2022-12-15 18:36:10 -0500

    renter history tables and maps are more dynamic

--- components/history/r_historyTable.tsx
+++ components/history/r_historyTable.tsx
@@ -13,15 +13,16 @@ const columns: GridColDef[] = [
   { field: 'startDateTime', type: 'dateTime', headerName: 'Start DateTime', width: 180 },
   { field: 'endDateTime', type: 'dateTime', headerName: 'End DateTime', width: 180 },
   { field: 'duration', type: 'number', headerName: 'Duration (hrs)', width: 100 },
-  { field: 'block', headerName: 'Block?', width: 70 },
+  { field: 'rebook', headerName: 'Rebook', width: 250, sortable: false, type: 'boolean' },
 ];
 
-var rows = [
-  { id: 1, name: 'John S', address: '123 Anywhere st', city: 'Austin', state: 'TX', zip: '78757', startDateTime: new Date('2022-04-25 8:00'), endDateTime: new Date('2022-04-25 12:00'), duration: '4 hours', block: 'X'},
-  { id: 2, name: 'Jimmy G', address: '101 Garage st', detail: '#304', city: 'Austin', state: 'TX', zip: '78754', startDateTime: new Date('2022-03-25 19:00'), endDateTime: new Date('2022-03-25 21:00'), duration: '2 hours', block: 'blocked'},
-  { id: 3, name: 'Sarah L', address: '99 Williams rd', detail: '#3', city: 'Austin', state: 'TX', zip: '78727', startDateTime: new Date('2022-04-25 12:00'), endDateTime: new Date('2022-04-25 13:00'), duration: '1 hour', block: 'X'},
-  { id: 4, name: 'Ernesto B', address: '55 MLK dr', city: 'Austin', state: 'TX', zip: '78727', startDateTime: new Date('2022-12-22 08:00'), endDateTime: new Date('2022-12-22 17:00'), duration: '9 hours', block: 'X'},
-  { id: 5, name: 'Ernesto B', address: '55 MLK dr', city: 'Austin', state: 'TX', zip: '78727', startDateTime: new Date('2022-12-23 10:00'), endDateTime: new Date('2022-12-23 17:00'), duration: '9 hours', block: 'X'},
+var rows: any[] = [
+  //Sample data below
+  // { id: 1, name: 'John S', address: '123 Anywhere st', city: 'Austin', state: 'TX', zip: '78757', startDateTime: new Date('2022-04-25 8:00'), endDateTime: new Date('2022-04-25 12:00'), duration: '4 hours', block: null},
+  // { id: 2, name: 'Jimmy G', address: '101 Garage st', detail: '#304', city: 'Austin', state: 'TX', zip: '78754', startDateTime: new Date('2022-03-25 19:00'), endDateTime: new Date('2022-03-25 21:00'), duration: '2 hours', block: true},
+  // { id: 3, name: 'Sarah L', address: '99 Williams rd', detail: '#3', city: 'Austin', state: 'TX', zip: '78727', startDateTime: new Date('2022-04-25 12:00'), endDateTime: new Date('2022-04-25 13:00'), duration: '1 hour', block: null},
+  // { id: 4, name: 'Ernesto B', address: '55 MLK dr', city: 'Austin', state: 'TX', zip: '78727', startDateTime: new Date('2022-12-22 08:00'), endDateTime: new Date('2022-12-22 17:00'), duration: '9 hours', block: null},
+  // { id: 5, name: 'Ernesto B', address: '55 MLK dr', city: 'Austin', state: 'TX', zip: '78727', startDateTime: new Date('2022-12-23 10:00'), endDateTime: new Date('2022-12-23 17:00'), duration: '9 hours', block: null},
 ];
 
 const diff_hours = (dt2: Date, dt1: Date) => {
@@ -30,16 +31,16 @@ const diff_hours = (dt2: Date, dt1: Date) => {
   return Math.abs(Math.round(diff));
 }
 
-export default function HistoryTable(props) {
+export default function HistoryTable(props: any) {
   if (props.listings.length !== 0) {
     rows = [];
-    props.listings.forEach((e, idx) => {
+    props.listings.forEach((e: any, idx: number) => {
       // console.log(e);
       const startDate = new Date(e.timeRangeStart);
       const endDate = new Date(e.timeRangeEnd);
       const diffDate = diff_hours(endDate, startDate);
       rows.push({id: e.id, name: `${e.firstName} ${e.lastName}`, address: e.address, detail: e.detail,
-      city: e.place_id, startDateTime: startDate, endDateTime: endDate, duration: diffDate, lat: e.lat, lng: e.lng});
+      city: e.place_id, startDateTime: startDate, endDateTime: endDate, duration: diffDate, lat: e.lat, lng: e.lng, rebook: e.rebook});
     });
   }
   return (
--- pages/renterHistory.tsx
+++ pages/renterHistory.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import NavBar from "../components/navBar";
 import HistoryTable from '../components/history/r_historyTable';
 import dynamic from 'next/dynamic'
 import { GridEventListener } from '@mui/x-data-grid';
@@ -11,9 +10,9 @@ class RenterHistory extends React.Component {
     this.state = {
       listings: [
         {id: 11, firstName: 'Matthew', lastName: 'McConaughey', place_id: 'Austin', lat: 30.2711286, lng: -97.7436995,
-        address: '123 Alright St', timeRangeStart: '2022-04-25 8:00', timeRangeEnd: '2022-04-25 12:00'},
+        address: '123 Alright St', timeRangeStart: '2022-04-25 8:00', timeRangeEnd: '2022-04-25 12:00', rebook: true},
         {id: 12, firstName: 'Dexter', lastName: 'Morgan', place_id: 'Miami', lat: 25.781681, lng: -80.211788,
-        address: '777 Tropicana Dr', detail: 'Spot #7', timeRangeStart: '2022-07-04 12:00', timeRangeEnd: '2022-07-05 12:00'},
+        address: '777 Tropicana Dr', detail: 'Spot #7', timeRangeStart: '2022-07-04 12:00', timeRangeEnd: '2022-07-05 12:00', rebook: false},
       ],
       userId: 100,
       currentLoc: [40.7128,-74.0060]
@@ -43,7 +42,6 @@ class RenterHistory extends React.Component {
     );
     return (
       <>
-        <NavBar />
         <h1>My Rental History</h1>
         <h3>Past Rentals</h3>
         <HistoryTable listings={this.state.listings} handleTableClick={this.handleTableClick}/>
