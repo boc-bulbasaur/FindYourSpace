89bceee00076ab4ff58c077a166596237ba187cb
Justo Miko Marquez <marquezmiko@gmail.com>
2022-12-26 22:44:38 -0500

    changed renterHistory to use dynamic data from db

--- pages/renterHistory.tsx
+++ pages/renterHistory.tsx
@@ -9,17 +9,9 @@ class RenterHistory extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      listings: [
-        {id: 11, name: 'Matthew McConaughey', place_id: 'Austin, TX', lat: 30.3111286, lng: -97.7336995,
-        address: '123 Alright St', timeRangeStart: '2022-04-25 8:00', timeRangeEnd: '2022-04-25 12:00', rebook: true},
-        {id: 102, name: 'Dexter Morgan', place_id: 'Miami, FL', lat: 25.781681, lng: -80.211788,
-        address: '777 Tropicana Dr', detail: 'Spot #7', timeRangeStart: '2022-07-04 12:00', timeRangeEnd: '2022-07-05 12:00', rebook: false},
-        {id: 131, name: 'Steve Rogers', place_id: 'Sunnyvale, CA', lat: 37.3688, lng: -122.0563,
-        address: '1234 USA St', detail: 'This parking spot is streetside parking. Please only park directly in front of the house addressed.', timeRangeStart: '2023-12-10 10:30', timeRangeEnd: '2023-12-10 14:30', rebook: true}
-      ],
-      userId: 100,
-      currentLoc: [40.7128,-74.0060],
-      currentLocStr: '',
+      listings: [],
+      user_id: '',
+      currentLoc: [40.7128,-74.0060], //default to NYC
       timeRange: '',
       numListings: ''
 
@@ -29,33 +21,107 @@ class RenterHistory extends React.Component {
   }
 
   componentDidMount(): void {
-    this.calculateTimeRange();
+    console.log("!!!RENTER HISTORY MOUNT!!!");
+    if (this.props.session !== undefined) {
+      this.setState({user_id: this.props.session.user.user_id});
+    }
+
+    if (this.props.session !== undefined) {
+      const getBookings = async () => {
+        // const user_id = 3;
+        await fetch(`/api/getBookings?user_id=${this.props.session.user.user_id}`, {
+          method: 'GET'
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('data', data);
+          if (data.warning === undefined) {
+            this.setState({
+              listings: data,
+              currentLoc: [data[0].lat, data[0].lng]
+            });
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+      }
+      getBookings();
+    } else {
+      this.setState({listings:
+        [
+          // {id: 11, name: 'Matthew McConaughey', place_id: 'Austin, TX', lat: 30.3111286, lng: -97.7336995,
+          // address: '123 Alright St', timeRangeStart: '2022-04-25 8:00', timeRangeEnd: '2022-04-25 12:00', rebook: true},
+          // {id: 102, name: 'Dexter Morgan', place_id: 'Miami, FL', lat: 25.781681, lng: -80.211788,
+          // address: '777 Tropicana Dr', detail: 'Spot #7', timeRangeStart: '2022-07-04 12:00', timeRangeEnd: '2022-07-05 12:00', rebook: false},
+          // {id: 131, name: 'Steve Rogers', place_id: 'Sunnyvale, CA', lat: 37.3688, lng: -122.0563,
+          // address: '1234 USA St', detail: 'This parking spot is streetside parking. Please only park directly in front of the house addressed.', timeRangeStart: '2023-12-10 10:30', timeRangeEnd: '2023-12-10 14:30', rebook: true}
+        ]
+      });
+    }
     if (this.state.listings.length > 0) {
       const firstLoc = this.state.listings[0];
       this.setState({currentLoc: [firstLoc.lat, firstLoc.lng]});
+      this.calculateTimeRange();
+    }
+  }
+
+  componentDidUpdate(prevProps: Readonly<{}>, prevState: Readonly<{}>, snapshot?: any): void {
+    if (this.state.user_id !== '' && prevState.count !== this.state.counta) {
+      console.log('!!!UPDATE!!!');
+      const getBookings = async () => {
+        // const user_id = 3;
+        await fetch(`/api/getBookings?user_id=${this.state.user_id}`, {
+          method: 'GET'
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('data', data);
+          if (data.warning === undefined) {
+            this.setState({
+              listings: data,
+              currentLoc: [data[0].lat, data[0].lng]
+            });
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+      }
+      getBookings();
+      if (this.state.listings.length > 0) {
+        const firstLoc = this.state.listings[0];
+        this.setState({currentLoc: [firstLoc.lat, firstLoc.lng]});
+        this.calculateTimeRange();
+      }
     }
   }
 
   calculateTimeRange() {
+    if (this.state.listings.length === 0) {
+      return;
+    }
     this.setState({numListings: this.state.listings.length});
     let earliestDT: Date, latestDT: Date;
-    this.state.listings.forEach((e, idx) => {
-      if (idx === 0) {
-        earliestDT = new Date(e.timeRangeStart);
-        latestDT = new Date(e.timeRangeEnd);
-      } else {
-        const currStartDT = new Date(e.timeRangeStart);
-        const currEndDT = new Date(e.timeRangeEnd);
-        if (currStartDT < earliestDT) {
-          earliestDT = currStartDT;
+    if (this.state.listings.length !== undefined) {
+      this.state.listings.forEach((e, idx) => {
+        if (idx === 0) {
+          earliestDT = new Date(e.timeRangeStart);
+          latestDT = new Date(e.timeRangeEnd);
+        } else {
+          const currStartDT = new Date(e.timeRangeStart);
+          const currEndDT = new Date(e.timeRangeEnd);
+          if (currStartDT < earliestDT) {
+            earliestDT = currStartDT;
+          }
+          if (currEndDT > latestDT) {
+            latestDT = currEndDT;
+          }
         }
-        if (currEndDT > latestDT) {
-          latestDT = currEndDT;
-        }
-      }
-    });
+      });
+    }
     //Calculate and return difference in hours
-    var diff =(latestDT.getTime() - earliestDT.getTime()) / 1000;
+    var diff = (latestDT.getTime() - earliestDT.getTime()) / 1000;
     diff /= (60 * 60);
     const totalHours = Math.abs(Math.round(diff));
     const days = Math.floor(totalHours/24);
