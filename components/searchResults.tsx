44cf7dfd416012eecb043d36e62412875302be54
Jingtian Liu <liujt1205@gmail.com>
2022-12-27 00:27:23 -0600

    some UI change

--- components/searchResults.tsx
+++ components/searchResults.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Flex } from '@chakra-ui/react'
 import SearchResult from './searchResult'
-import { Skeleton, Stack, ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { Box, Grid, Skeleton, Stack, ToggleButton, ToggleButtonGroup } from '@mui/material';
+import styles from '../styles/search.module.css';
+
 
 type SearchResultsProps = {
   results: {}[];
@@ -11,25 +12,25 @@ type SearchResultsProps = {
 }
 
 const SearchResults = ({results, isLoading, sortBy, setSortBy}: SearchResultsProps) =>{
-  const handleChange = (e: React.MouseEvent<HTMLElement>, newSortBy: String) => {
-    if (newSortBy !== null) {
-      setSortBy(newSortBy);
+  const handleChange = (e: React.MouseEvent<HTMLElement>) => {
+    if (e.target.value !== null) {
+      setSortBy(e.target.value);
     }
   }
 
   if (isLoading) return (
-    <Flex
-      direction={'column'}
-      bg={'whiteAlpha.900'}
-      width={'35%'}
-      height ={'100%'}
-      position ={'absolute'}
-      left={0}
-      top={0}
-      zIndex={1}
-      overflow='hidden'
-      px={2}
-    >
+    <Box sx={{
+      direction: 'column',
+      bg: 'whiteAlpha.900',
+      width: '38%',
+      height: '100%',
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      zIndex: 1,
+      overflow: 'hidden',
+      px: 2,
+    }} >
       <Stack>
         <br></br>
         <Skeleton height='50px' />
@@ -51,34 +52,30 @@ const SearchResults = ({results, isLoading, sortBy, setSortBy}: SearchResultsPro
         <Skeleton height='50px' />
         <br></br>
         <Skeleton height='50px' />
-        {/* <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' /> */}
       </Stack>
-    </Flex>
+    </Box>
   )
   return (
-    <Flex
-      direction={'column'}
-      bg={'whiteAlpha.900'}
-      width={'35%'}
-      height ={'100%'}
-      position ={'absolute'}
-      left={0}
-      top={0}
-      zIndex={1}
-      overflow='hidden'
-      px={2}
-    >
-      <Flex
-        justifyContent={'right'}
-        alignItems={'center'}
-      >
-        <Flex
+    <Box sx={{
+      direction: 'column',
+      bg: 'whiteAlpha.900',
+      width: '38%',
+      height: '100%',
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      zIndex: 1,
+      overflow: 'scroll',
+      px: 2,
+    }} className={styles.searchresults} >
+      <Grid container direction={'row'} justifyContent={'flex-end'} alignItems={'center'} >
+        <Grid
           justifyContent={'center'}
           alignItems={'center'}
         >
           Sort By:
-        </Flex>
-        <Flex
+        </Grid>
+        <Grid
           justifyContent={'center'}
           alignItems={'center'}
         >
@@ -89,22 +86,26 @@ const SearchResults = ({results, isLoading, sortBy, setSortBy}: SearchResultsPro
             aria-label="sortBy"
             onChange={handleChange}
           >
-            <ToggleButton value="lat">Distance</ToggleButton>
-            <ToggleButton value="distance">Price</ToggleButton>
+            <ToggleButton value="distance">Distance</ToggleButton>
+            <ToggleButton value="price">Price</ToggleButton>
           </ToggleButtonGroup>
-        </Flex>
-      </Flex>
-      <Flex flex={1} overflowY={'scroll'} mt ={16} direction={'column'} className='search-results'>
+        </Grid>
+      </Grid>
+      <Grid container direction={'column'} columnSpacing={2} >
         {
           results.length !== 0 && results.map((location: Object): JSX.Element => {
-            return <SearchResult location={location} />
+            return (
+              <Grid key={location.id}>
+                <SearchResult location={location} />
+              </Grid>
+            )
           })
         }
         {
           results.length === 0 && <div>No available results</div>
         }
-      </Flex>
-    </Flex>
+      </Grid>
+    </Box>
   )
 }
 
